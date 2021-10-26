534c97bd1a834571622012d6ab69d68a189b1b65
Luiz Henrique <Luiz_HB@live.com>
2021-10-26 09:47:20 -0300

    Dom

---
+++ DOM/script.js
@@ -0,0 +1,12 @@
+// getElementById()
+
+// document.getElementById('dom-title')
+// console.log(document.getElementById('dom-title'))
+
+// const element = document.getElementById('dom-title')
+// console.log(element)
+
+//getElementByClassName()
+
+const element = document.getElementsByClassName('one')
+console.log(element)
